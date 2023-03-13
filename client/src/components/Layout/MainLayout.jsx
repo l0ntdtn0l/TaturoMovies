4476294a3e460c9aeb9bcd6be1da881112b2e0fa
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-03-13 13:30:56 +0700

    Frontend: create GlobalLoading!

--- client/src/components/Layout/MainLayout.jsx
+++ client/src/components/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import GlobalLoading from '../common/GlobalLoading';
 
 const MainLayout = () => {
     return (
         <>
             {/* Global loading */}
+            <GlobalLoading />
             {/* Global loading */}
 
             {/* login modal */}
